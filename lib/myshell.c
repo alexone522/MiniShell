5e14c285db9f500f855aeccfee6752d26dd0981d
eLNiTa <e.arcaz.2022@alumnos.urjc.es>
2024-12-11 13:46:55 -0500

    :)

--- lib/myshell.c
+++ lib/myshell.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include "parser.h"
 
-int main(){
+#define BUFF 1024
 
+int main(){
+    
+    printf("msh> "); //prompt
+    char input[BUFF];
+    
+
+    //Ignora las señales:
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+
+    
     return 0;
 }
