7fff3a14fb31bb8fa79731c47efe5088a2c62832
eLNiTa <e.arcaz.2022@alumnos.urjc.es>
2024-12-11 14:26:17 -0500

    miniUpdate pt.2

--- lib/myshell.c
+++ lib/myshell.c
@@ -5,16 +5,31 @@
 
 #define BUFF 1024
 
-int main(){
-    
+int main(){    
     printf("msh> "); //prompt
-    char input[BUFF];
+    char input[BUFF];//buffer
     
 
     //Ignora las señales:
     signal(SIGINT, SIG_IGN);
     signal(SIGQUIT, SIG_IGN);
 
+    while(fgets(input, BUFF, stdin)){ //lee de teclado en cada iteración
+        tline * linea = tokenize(input);//tokeniza
+
+        if(linea->ncommands == 0){ 
+            //si no tiene comandos imprime el prompt y espera otro comando
+            printf("msh> ");
+            continue;
+        }
+        if(linea->ncommands == 1){
+            //caso de 1 mandato
+        } else if (linea->ncommands == 2){
+            //2 mandatos
+        } else {
+            //+ de 2 mandatos
+        }
+    }
     
     return 0;
 }
